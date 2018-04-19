18e35b45081b5a7ecf6fff2f72ff4fc970911e34
Michael Horsley <michael.horsley@sorted.com>
2018-04-19 09:03:23 +0100

    Added spacing, removed comments

--- controller.go
+++ controller.go
@@ -21,27 +21,37 @@ type Controller struct {
 // Index GET /
 func (c *Controller) Index(w http.ResponseWriter, r *http.Request) {
 	leagues := c.Repository.GetLeagues()
+
 	log.Println(leagues)
+
 	data, _ := json.Marshal(leagues)
+
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
+
 	w.WriteHeader(http.StatusOK)
+
 	w.Write(data)
+
 	return
 }
 
 // AddLeague POST /
 func (c *Controller) AddLeague(w http.ResponseWriter, r *http.Request) {
 	var league League
+
 	body, err := ioutil.ReadAll(io.LimitReader(r.Body, 1048576)) // read the body of the request
+
 	if err != nil {
 		log.Fatalln("Error AddLeague", err)
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
+
 	if err := r.Body.Close(); err != nil {
 		log.Fatalln("Error AddLeague", err)
 	}
+
 	if err := json.Unmarshal(body, &league); err != nil { // unmarshall body contents as a type Candidate
 		w.WriteHeader(422) // unprocessable entity
 		if err := json.NewEncoder(w).Encode(err); err != nil {
@@ -50,13 +60,17 @@ func (c *Controller) AddLeague(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 	}
-	success := c.Repository.AddLeague(league) // adds the league to the DB
+
+	success := c.Repository.AddLeague(league)
+
 	if !success {
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
+
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 	w.WriteHeader(http.StatusCreated)
+
 	return
 }
 
@@ -68,14 +82,17 @@ func (c *Controller) UpdateLeague(w http.ResponseWriter, r *http.Request) {
 	var league League
 
 	body, err := ioutil.ReadAll(io.LimitReader(r.Body, 1048576)) // read the body of the request
+
 	if err != nil {
 		log.Fatalln("Error UpdateLeague", err)
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
+
 	if err := r.Body.Close(); err != nil {
 		log.Fatalln("Error AddaUpdateLeaguelbum", err)
 	}
+
 	if err := json.Unmarshal(body, &league); err != nil { // unmarshall body contents as a type Candidate
 		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 		w.WriteHeader(422) // unprocessable entity
@@ -106,7 +123,8 @@ func (c *Controller) UpdateLeague(w http.ResponseWriter, r *http.Request) {
 // DeleteLeague DELETE /
 func (c *Controller) DeleteLeague(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
-	id := vars["id"]                                     // param id
+	id := vars["id"]
+
 	if err := c.Repository.DeleteLeague(id); err != "" { // delete a league by id
 		if strings.Contains(err, "404") {
 			w.WriteHeader(http.StatusNotFound)
@@ -115,8 +133,10 @@ func (c *Controller) DeleteLeague(w http.ResponseWriter, r *http.Request) {
 		}
 		return
 	}
+
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.WriteHeader(http.StatusOK)
+
 	return
 }
--- repository.go
+++ repository.go
@@ -23,58 +23,74 @@ const DOCNAME = "leagues"
 // GetLeagues returns the list of Leagues
 func (r Repository) GetLeagues() Leagues {
 	session, err := mgo.Dial(SERVER)
+
 	if err != nil {
 		fmt.Println("Failed to establish connection to Mongo server:", err)
 	}
+
 	defer session.Close()
+
 	c := session.DB(DBNAME).C(DOCNAME)
+
 	results := Leagues{}
+
 	if err := c.Find(nil).All(&results); err != nil {
 		fmt.Println("Failed to write results:", err)
 	}
+
 	return results
 }
 
 // AddLeague inserts an League in the DB
 func (r Repository) AddLeague(league League) bool {
 	session, err := mgo.Dial(SERVER)
+
 	defer session.Close()
+
 	league.ID = bson.NewObjectId()
+
 	session.DB(DBNAME).C(DOCNAME).Insert(league)
+
 	if err != nil {
 		log.Fatal(err)
 		return false
 	}
+
 	return true
 }
 
 // UpdateLeague updates an League in the DB (not used for now)
 func (r Repository) UpdateLeague(league League) bool {
 	session, err := mgo.Dial(SERVER)
+
 	defer session.Close()
+
 	session.DB(DBNAME).C(DOCNAME).UpdateId(league.ID, league)
+
 	if err != nil {
 		log.Fatal(err)
 		return false
 	}
+
 	return true
 }
 
 // DeleteLeague deletes an League (not used for now)
 func (r Repository) DeleteLeague(id string) string {
 	session, err := mgo.Dial(SERVER)
+
 	defer session.Close()
-	// Verify id is ObjectId, otherwise bail
+
 	if !bson.IsObjectIdHex(id) {
 		return "NOT FOUND"
 	}
-	// Grab id
+
 	oid := bson.ObjectIdHex(id)
-	// Remove user
+
 	if err = session.DB(DBNAME).C(DOCNAME).RemoveId(oid); err != nil {
 		log.Fatal(err)
 		return "INTERNAL ERR"
 	}
-	// Write status
+
 	return "OK"
 }
