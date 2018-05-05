3980870703ba13aa8438de98f15c58956a1d40ef
Michael Horsley <omega_mike@hotmail.com>
2018-05-05 17:43:42 +0100

    Move Routes out into Module

---
+++ RouteModule.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+
+	"poolgolang/infrastructure"
+	"poolgolang/league"
+	"poolgolang/participant"
+)
+
+func NewRouter() *mux.Router {
+	router := mux.NewRouter().StrictSlash(true)
+
+	var allRoutes infrastructure.Routes
+
+	allRoutes = append(allRoutes, participant.GetRoutes()...)
+	allRoutes = append(allRoutes, league.GetRoutes()...)
+
+	for _, route := range allRoutes {
+		var handler http.Handler
+		handler = route.HandlerFunc
+		router.
+			Methods(route.Method).
+			Path(route.Pattern).
+			Name(route.Name).
+			Handler(handler)
+	}
+
+	return router
+}
--- main.go
+++ main.go
@@ -5,11 +5,6 @@ import (
 	"net/http"
 
 	"github.com/gorilla/handlers"
-	"github.com/gorilla/mux"
-
-	"poolgolang/infrastructure"
-	"poolgolang/league"
-	"poolgolang/participant"
 )
 
 func main() {
@@ -19,25 +14,4 @@ func main() {
 	allowedMethods := handlers.AllowedMethods([]string{"GET", "POST", "DELETE", "PUT"})
 
 	log.Fatal(http.ListenAndServe(":8080", handlers.CORS(allowedOrigins, allowedMethods)(foo)))
-}
-
-func NewRouter() *mux.Router {
-	router := mux.NewRouter().StrictSlash(true)
-
-	var allRoutes infrastructure.Routes
-
-	allRoutes = append(allRoutes, participant.GetRoutes()...)
-	allRoutes = append(allRoutes, league.GetRoutes()...)
-
-	for _, route := range allRoutes {
-		var handler http.Handler
-		handler = route.HandlerFunc
-		router.
-			Methods(route.Method).
-			Path(route.Pattern).
-			Name(route.Name).
-			Handler(handler)
-	}
-
-	return router
 }
